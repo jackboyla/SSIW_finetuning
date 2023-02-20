28f15874c21d8b030cd3b34ae7b11f8dab778b68
Jack Boylan <jack.boylan25@mail.dcu.ie>
2023-02-20 13:25:24 +0000

    Replaces Test_Minist with src folder

---
+++ src/utils/decode_heads/__init__.py
@@ -0,0 +1,8 @@
+from .aspp_head import ASPPHead
+from .fcn_head import FCNHead
+from .fpn_head import FPNHead
+from .psp_head import PSPHead
+from .segformer_head import SegFormerHead
+from .uper_head import UPerHead
+
+__all__ = ["FCNHead", "ASPPHead", "FPNHead", "PSPHead", "SegFormerHead"]
