ea8f74740c31fc02f216f9d1e38a206d8fba35af
Jack Boylan <jack.boylan25@mail.dcu.ie>
2023-02-20 13:40:30 +0000

    Adds requirements.txt and script to download CMP Facade data

---
+++ download.sh
@@ -0,0 +1,12 @@
+wget https://cmp.felk.cvut.cz/~tylecr1/facade/CMP_facade_DB_base.zip
+wget https://cmp.felk.cvut.cz/~tylecr1/facade/CMP_facade_DB_extended.zip
+mkdir data
+mkdir data/base
+mkdir data/extended
+unzip CMP_facade_DB_base.zip -d data/base
+unzip CMP_facade_DB_extended.zip -d data/extended
+rm CMP_facade_DB_base.zip
+rm CMP_facade_DB_extended.zip
+wget https://cloudstor.aarnet.edu.au/plus/s/gXaGsZyvoUwu97t/download -O universal_cat2vec.npy
+wget https://cloudstor.aarnet.edu.au/plus/s/AtYYaVSVVAlEwve/download -O segformer_7data.pth
+pip install requirements.txt
